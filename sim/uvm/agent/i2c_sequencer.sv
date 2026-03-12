dd58498a23505f90da934405b82b1355d1e1f11f
Live-Yourself <huhuiheng0329@qq.com>
2026-03-12 10:33:56 +0800

    agent

---
+++ sim/uvm/agent/i2c_sequencer.sv
@@ -0,0 +1,7 @@
+﻿class i2c_sequencer extends uvm_sequencer#(i2c_item);
+  `uvm_component_utils(i2c_sequencer)
+
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction
+endclass
