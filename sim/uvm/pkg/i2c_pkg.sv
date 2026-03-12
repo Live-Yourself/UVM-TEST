c7944c14bb06e618eba9a176b1a9197dcc1da04f
Live-Yourself <huhuiheng0329@qq.com>
2026-03-12 10:42:52 +0800

    package

---
+++ sim/uvm/pkg/i2c_pkg.sv
@@ -0,0 +1,15 @@
+﻿package i2c_pkg;
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+
+  `include "i2c_cfg.sv"
+  `include "i2c_item.sv"
+  `include "i2c_sequencer.sv"
+  `include "i2c_seqs.sv"
+  `include "i2c_driver.sv"
+  `include "i2c_monitor.sv"
+  `include "i2c_agent.sv"
+  `include "i2c_scoreboard.sv"
+  `include "i2c_env.sv"
+  `include "i2c_tests.sv"
+endpackage
