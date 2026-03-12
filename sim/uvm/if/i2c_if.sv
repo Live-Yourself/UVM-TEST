89aa07d744ca571b24370e81214d3759743bedaf
Live-Yourself <huhuiheng0329@qq.com>
2026-03-12 10:42:18 +0800

    Interface

---
+++ sim/uvm/if/i2c_if.sv
@@ -0,0 +1,18 @@
+﻿interface i2c_if;
+  logic clk;
+  logic rst_n;
+
+  logic scl;
+  logic sda_drv_low;
+  logic sda_oe_dut;
+  tri1  sda;
+  logic sda_in;
+
+  assign sda    = (sda_drv_low || sda_oe_dut) ? 1'b0 : 1'bz;
+  assign sda_in = sda;
+
+  task init_bus();
+    scl         = 1'b1;
+    sda_drv_low = 1'b0;
+  endtask
+endinterface
